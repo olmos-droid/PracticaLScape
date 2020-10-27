4507725a57a6b7694cd0246c85b9331b3acec03b
iam47714462 <olmosescolatreball@gmail.com>
2020-10-27 16:24:15 +0100

    hooks fets

--- app/src/main/java/com/example/practicalscape/MainActivity.java
+++ app/src/main/java/com/example/practicalscape/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.practicalscape;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView image1;
+    private ImageView image2;
+    private ImageView image3;
+    private ImageView image4;
+    private ImageView image5;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image1=(ImageView) findViewById(R.id.image1);
+        image2=(ImageView) findViewById(R.id.image2);
+        image3=(ImageView) findViewById(R.id.image3);
+        image4=(ImageView) findViewById(R.id.image4);
+        image5=(ImageView) findViewById(R.id.image5);
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        image4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        image5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
